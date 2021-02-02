43d36d171e5cabff800abdd7c62d391beacace2f
srmarcballestero <marconiballestero@gmail.com>
2021-02-02 21:31:59 +0100

    Exercici 2

--- Exercici 2 - Coeficients de Fresnel en funció de l'angle d'incidència/Fresnel.py
+++ Exercici 2 - Coeficients de Fresnel en funció de l'angle d'incidència/Fresnel.py
@@ -3,7 +3,7 @@
 """
 Exercici 2 d'avaluació continuada.
  - Mòdul: Fresnel.py
- - Revisió: 15/09/2020
+ - Revisió: 02/02/2021
 
  NOTES:
     - En tot el codi es treballa en unitats del SI (inclosos angles en radians)
@@ -15,10 +15,9 @@ import matplotlib.pyplot as plt
 
 """
 Variables globals
-    ld: float                # longitud d'ona calculada a partir del DNI
-    n: float                 # índex de refracció de ld en un vidre N-BK7 (font: http://refractiveindex.info)
+    n1: float                # índex de refracció del medi 1 (aire 1.000)
+    n2: float                # índex de refracció del medi 2 (vidre N-BK7 per lambda = 440 nm, font: http://refractiveindex.info)
 """
-ld = (20 * (39418004 % 17) + 400) * 1e-9        # ld = 440 nmç
 n1 = 1.
 n2 = 1.5263
 
@@ -39,17 +38,9 @@ def isOverCritical(phi1, n1, n2):
         return False
 
 
-def isNormal(phi1):
-    """Comprova si hi ha incidència normal."""
-    if phi1 == 0.:
-        return True
-    else:
-        return False
-
-
 def refractionAngle(phi1, n1, n2):
     """Retorna l'angle que forma amb la normal el raig transmès en una refracció."""
-    if isNormal(phi1):                  # incidència normal
+    if phi1 == 0.:                              # incidència normal
         return 0.
     elif isOverCritical(phi1, n1, n2):          # reflexió total
         pass
@@ -61,11 +52,11 @@ def tParallel(phi1, n1, n2):
     """Retorna coeficient de transmissió en la component paral·lela."""
     phi2 = refractionAngle(phi1, n1, n2)
 
-    if phi2 == 0:
+    if phi2 == 0:                               # incidència normal
         return 2*n1 / (n1+n2)
-    elif isOverCritical(phi1, n1, n2):
+    elif isOverCritical(phi1, n1, n2):          # reflexió total
         return 0.
-    else:
+    else:                                       # transmissió estàndard
         return 2*sin(phi2)*cos(phi1) / (sin(phi1 + phi2)*cos(phi2 - phi1))
 
 
@@ -73,11 +64,11 @@ def tPerpendicular(phi1, n1, n2):
     """Retorna coeficient de transmissió en la component perpendicular."""
     phi2 = refractionAngle(phi1, n1, n2)
 
-    if phi2 == 0:
+    if phi2 == 0:                               # incidència normal
         return 2*n1 / (n1+n2)
-    elif isOverCritical(phi1, n1, n2):
+    elif isOverCritical(phi1, n1, n2):          # reflexió total
         return 0.
-    else:
+    else:                                       # transmissió estàndard
         return 2*sin(phi2)*cos(phi1) / sin(phi1 + phi2)
 
 
@@ -85,13 +76,13 @@ def rParallel(phi1, n1, n2):
     """Retorna coeficient de reflexió en la component paral·lela."""
     phi2 = refractionAngle(phi1, n1, n2)
 
-    if isBrewster(phi1, n1, n2):                   # condició d'incidència en l'angle de Brewster
+    if isBrewster(phi1, n1, n2):                 # incidència en l'angle de Brewster
         return 0.
-    elif isOverCritical(phi1, n1, n2):
+    elif isOverCritical(phi1, n1, n2):           # reflexió total
         return -1.
-    elif phi2 == 0:
+    elif phi2 == 0:                              # incidència normal
         return (n1-n2) / (n1+n2)
-    else:
+    else:                                        # reflexió estàndard
         return tan(phi2 - phi1) / tan(phi2 + phi1)
 
 
@@ -99,30 +90,55 @@ def rPerpendicular(phi1, n1, n2):
     """Retorna coeficient de reflexió en la component paral·lela."""
     phi2 = refractionAngle(phi1, n1, n2)
 
-    if isOverCritical(phi1, n1, n2):
+    if isOverCritical(phi1, n1, n2):             # reflexió total
         return 1.
-    elif phi2 == 0:
+    elif phi2 == 0:                              # incidència normal
         return (n1-n2) / (n1+n2)
-    else:
+    else:                                        # reflexió estàndard
         return sin(phi2 - phi1) / sin(phi2 + phi1)
 
 
-phi = np.linspace(0, np.pi/2., num=500)
-fresnel_coeffs = np.zeros((4, 500))
-for n, phi1 in enumerate(phi):
-    phi2 = refractionAngle(phi1, 1.000, n)
-    fresnel_coeffs[0, n] = tParallel(phi1, n1, n2)
-    fresnel_coeffs[1, n] = tPerpendicular(phi1, n1, n2)
-    fresnel_coeffs[2, n] = rParallel(phi1, n1, n2)
-    fresnel_coeffs[3, n] = rPerpendicular(phi1, n1, n2)
+N = 500                                          # nombre d'iteracions (resoució en phi1)
+phi = np.linspace(0, np.pi/2., num=N)            # valors de phi1
+t_parallel = np.zeros(N)                         # coef. de transmissió paral·lela
+t_perpendicular = np.zeros(N)                    # coef. de transmissió perpendicular
+r_parallel = np.zeros(N)                         # coef. de reflexió paral·lela
+r_perpendicular = np.zeros(N)                    # coef. de reflexió perpendicular
+
+"""Calcula els coeficients de Fresnel per als angles d'incidència de l'array phi"""
+for i, phi1 in enumerate(phi):
+    t_parallel[i] = tParallel(phi1, n1, n2)
+    t_perpendicular[i] = tPerpendicular(phi1, n1, n2)
+    r_parallel[i] = rParallel(phi1, n1, n2)
+    r_perpendicular[i] = rPerpendicular(phi1, n1, n2)
+
+"""
+Calcula diferència entre els coeficients de transmissió paral·lels i perpendiculars
+íd. per als de reflexió
+"""
+t_diff = t_parallel - t_perpendicular
+r_diff = r_parallel - r_perpendicular
 
 
-plt.plot(phi, fresnel_coeffs[0, :], color="red")
-plt.plot(phi, fresnel_coeffs[1, :], color="green")
-plt.plot(phi, fresnel_coeffs[2, :], color="blue")
-plt.plot(phi, fresnel_coeffs[3, :], color="orange")
+"""
+Aproxima els coeficients de transmissió i reflexió per la mitjana en les components
+i estudia en quin rang l'error és inferior al 5%
+"""
+t_approx = .5 * (t_parallel+t_perpendicular)
+r_approx = .5 * (r_parallel+r_perpendicular)
 
-plt.plot(phi, fresnel_coeffs[0, :]-fresnel_coeffs[1, :], color="cyan")
-plt.plot(phi, fresnel_coeffs[2, :]-fresnel_coeffs[3, :], color="yellow")
 
+
+
+plt.plot(phi, t_parallel, color="red")
+plt.plot(phi, t_perpendicular, color="green")
+plt.plot(phi, t_approx, color="blue")
+"""
+plt.plot(phi, r_parallel, color="blue")
+plt.plot(phi, r_perpendicular, color="orange")
+"""
+"""
+plt.plot(phi, t_diff, color="cyan")
+plt.plot(phi, r_diff, color="yellow")
+"""
 plt.show()
